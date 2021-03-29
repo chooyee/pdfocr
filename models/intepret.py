f82b0ece9cd528b65232154cff63c359c68f1893
unknown <chooyee@gmail.com>
2021-03-29 22:23:39 +0800

    change spacy loading method

--- models/intepret.py
+++ models/intepret.py
@@ -2,7 +2,7 @@ import pandas as pd
 import csv
 import spacy
 from infra.logger import Logger
-
+import en_core_web_trf
 # print(dataframe)
 # for ind in dataframe.index:
 #     print(dataframe[3][ind])
@@ -30,7 +30,9 @@ def read_text(dataframe):
 def find_name(str):
     bankName = []
     bankAcc = []
-    nlp = spacy.load("en_core_web_trf") 
+    
+    nlp = en_core_web_trf.load()
+    #nlp = spacy.load("en_core_web_trf") 
     # nlp = spacy.load("en_core_web_sm") 
     ruler = nlp.add_pipe("entity_ruler")
     patterns = [{"label": "ORG", "pattern": "AmBank (M) Berhad"},
